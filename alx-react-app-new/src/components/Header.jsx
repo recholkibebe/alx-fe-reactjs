b843e935e222a1bc4d9becf5347eeb3506ee2709
recholkibebe <recholkibebe@gmail.com>
2025-07-18 16:39:08 +0300

    styling

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,13 +1,12 @@
 // src/components/Header.jsx
-// import React from 'react';
-//
-// const Header = () => {
-//   return (
-//       <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '20px' }}>
-//             <h1>My Favorite Cities</h1>
-//                 </header>
-//                   );
-//                   };
-//
-//                   export default Header;
-//
+ import React from 'react';
+
+ const Header = () => {
+   return (
+       <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '20px' }}>
+             <h1>My Favorite Cities</h1>
+                 </header>
+                   );
+                   };
+
+                   export default Header;
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,16 +1,16 @@
 // src/components/UserProfile.jsx
-//
-// import React from 'react';
-//
-// const UserProfile = ({ name, age, bio }) => {
-//   return (
-//       <div style={{ border: '1px solid gray', padding: '15px', margin: '15px', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
-//             <h2 style={{ color: 'blue', fontSize: '1.5rem' }}>{name}</h2>
-//                   <p>Age: <span style={{ fontWeight: 'bold' }}>{age}</span></p>
-//                         <p>Bio: <span style={{ fontStyle: 'italic' }}>{bio}</span></p>
-//                             </div>
-//                               );
-//                               };
-//
-//                               export default UserProfile;
-//
+
+ import React from 'react';
+
+ const UserProfile = ({ name, age, bio }) => {
+   return (
+       <div style={{ border: '1px solid gray', padding: '15px', margin: '15px', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
+             <h2 style={{ color: 'blue', fontSize: '1.5rem' }}>{name}</h2>
+                   <p>Age: <span style={{ fontWeight: 'bold' }}>{age}</span></p>
+                         <p>Bio: <span style={{ fontStyle: 'italic' }}>{bio}</span></p>
+                             </div>
+                               );
+                               };
+
+                               export default UserProfile;
+                               
