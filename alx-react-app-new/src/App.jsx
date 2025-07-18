a8ed5c36728156c5535a8089eadea5f24e9c6a14
recholkibebe <recholkibebe@gmail.com>
2025-07-18 16:47:00 +0300

    comments

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,19 +1,19 @@
 // src/App.jsx
 //
-// import React from 'react';
-// import Header from './components/Header';
-// import MainContent from './components/MainContent';
-// import Footer from './components/Footer';
-//
-// function App() {
-//   return (
-//       <>
-//             <Header />
-//                   <MainContent />
-//                         <Footer />
-//                             </>
-//                               );
-//                               }
-//
-//                               export default App;
-//
+ import React from 'react';
+ import Header from './components/Header';
+ import MainContent from './components/MainContent';
+ import Footer from './components/Footer';
+
+ function App() {
+   return (
+       <>
+             <Header />
+                   <MainContent />
+                         <Footer />
+                             </>
+                               );
+                               }
+
+                               export default App;
+
