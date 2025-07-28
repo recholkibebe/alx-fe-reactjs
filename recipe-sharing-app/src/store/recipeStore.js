d8d6c5af20b574eb9553d2c95dcdf8629afbb908
recholkibebe <recholkibebe@gmail.com>
2025-07-28 11:59:42 +0300

    comments out

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,11 +1,10 @@
- src/store/recipeStore.js
- import { create } from 'zustand';
+import { create } from 'zustand';
 
- export const useRecipeStore = create((set) => ({
-   recipes: [],
-     addRecipe: (newRecipe) =>
-         set((state) => ({
-               recipes: [...state.recipes, newRecipe],
-                   })),
-                     setRecipes: (recipes) => set({ recipes }),
-                     }));
+export const useRecipeStore = create((set) => ({
+	  recipes: [],
+	  addRecipe: (newRecipe) =>
+	    set((state) => ({
+		          recipes: [...state.recipes, newRecipe],
+		        })),
+}));
+
